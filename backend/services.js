c35da0a848de01d5de91cb09a622a81f9410ec98
Jayathra Abeywarna <jayathra@users.noreply.github.com>
2025-08-11 13:14:54 -0400

    Started working on the llm chunking verification

--- backend/services.js
+++ backend/services.js
@@ -37,17 +37,19 @@ const chunkSchema = z.object({
     )
 });
 
-export const parseFiles = async (files) => {
+const parseFiles = async (files) => {
     const parsedPdfs = []
-    const ids = []
 
     for (const file of files) {
         const data = await pdf(file.buffer);
         parsedPdfs.push(data.text) 
-        ids.push(crypto.randomUUID())
     }
 
-    return { parsedPdfs, ids }
+    return parsedPdfs
+}
+
+const verifyChunks = (chunkArray, fullText) => {
+    return chunkArray.every(chunk => fullText.includes(chunk.content))
 }
 
 export const llmChunking = async (devPrompt, userPrompt) => {
@@ -75,17 +77,21 @@ export const llmChunking = async (devPrompt, userPrompt) => {
 
 export const processor = async (files, jobPostingText) => {
     
-    const { parsedPdfs, ids } = await parseFiles(files)
+    const parsedPdfs = await parseFiles(files)
+    const concatParsedPdfs = parsedPdfs.join("\n\nNext Document\n\n")
 
-    const userPromptDocs = `Please chunk the following career documents:\n\n${parsedPdfs.join("\n\nNext Document\n\n")}`
+    const userPromptDocs = `Please chunk the following career documents:\n\n${concatParsedPdfs}`
     const docChunks = await llmChunking(DOC_CHUNKING_DEVELOPER_PROMPT, userPromptDocs)
+    console.log(verifyChunks(docChunks, concatParsedPdfs))
 
     const userPromptJobPosting = `Please chunk the following job posting:\n\n${jobPostingText}`
     const jobPostingChunks = await llmChunking(JOB_POSTING_CHUNKING_DEVELOPER_PROMPT, userPromptJobPosting)
-    
-    console.log(docChunks)
+    console.log(verifyChunks(jobPostingChunks, jobPostingText))
 
-    console.log(jobPostingChunks)
+    // console.log(docChunks)
+
+    // console.log(jobPostingChunks)
 
     return { docChunks, jobPostingChunks }
 }
+
