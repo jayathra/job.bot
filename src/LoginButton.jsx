3246c71da386a884f3fc3aae3412dfa7c72195c5
Brian Peiris <79419+brianpeiris@users.noreply.github.com>
2025-08-24 04:37:30 -0400

    blank slate

--- src/LoginButton.jsx
+++
@@ -1,9 +0,0 @@
-import Button from "@mui/material/Button";
-
-export default function LoginButton({ loginHandler }) {
-  return (
-    <Button variant="contained" onClick={loginHandler}>
-      Login
-    </Button>
-  );
-}
